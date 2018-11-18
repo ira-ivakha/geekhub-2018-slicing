e7305a927f08076f36d2524376ffb3c6ae7287c6
Iryna Ivakha <cs120879div1@gmail.com>
2018-11-18 15:14:10 +0200

    Template slicing

---
+++ gulpfile.js
@@ -0,0 +1,28 @@
+var gulp            = require('gulp');
+var browserSync     = require('browser-sync').create();
+var sass            = require('gulp-sass');
+var postcss         = require('gulp-postcss')
+var autoprefixer    = require('autoprefixer')
+
+
+// Static Server + watching scss/html files
+gulp.task('serve', function() {
+
+    browserSync.init({
+        server: "./"
+    });
+
+    gulp.watch("scss/**/*.scss", ['sass']);
+    gulp.watch("./*.html").on('change', browserSync.reload);
+});
+
+// Compile sass into CSS & auto-inject into browsers
+gulp.task('sass', function() {
+    return gulp.src("scss/styles.scss")
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest("./"))
+        .pipe(postcss([ autoprefixer() ]))
+        .pipe(browserSync.stream());
+});
+
+gulp.task('default', ['sass', 'serve']);
